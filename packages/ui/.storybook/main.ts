6a7edb9fbc8f1f6739e69b21afafaabec0a8e49c
Philipp Litzenberger <philipp@bendcircular.com>
2024-08-01 15:47:06 +0200

    feat(design-system): enable svgr

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,9 +2,10 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { mergeConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
-  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
@@ -13,7 +14,8 @@ const config: StorybookConfig = {
 
   viteFinal: async (config) =>
     mergeConfig(config, {
-      plugins: [nxViteTsPaths()],
+      plugins: [nxViteTsPaths(), svgr()],
+      define: { 'process.env': {} },
     }),
 };
 
---
+++ packages/ui/src/atoms/logo/index.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Logo } from './index';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof Logo> = {
+  component: Logo,
+  title: 'atoms/Logo',
+};
+export default meta;
+type Story = StoryObj<typeof Logo>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Welcome to Logo!/gi)).toBeTruthy();
+  },
+};
