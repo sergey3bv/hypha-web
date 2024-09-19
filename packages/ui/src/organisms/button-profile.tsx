17dad862638edc147a12363b9b7bffb22ba638fc
Philipp Litzenberger <philipp@bendcircular.com>
2024-09-19 16:50:18 +0200

    feat(MenuTop): update layout

---
+++ packages/ui/src/organisms/button-profile.tsx
@@ -0,0 +1,17 @@
+import { Avatar, AvatarImage, Button } from '../atoms';
+
+export type ButtonProfileProps = {
+  avatarSrc: string;
+  userName: string;
+};
+
+export const ButtonProfile = ({ avatarSrc, userName }: ButtonProfileProps) => {
+  return (
+    <Button variant="secondary" className="flex items-center space-x-4 p-1 pr-6 rounded-full">
+      <Avatar className="w-8 h-8">
+        <AvatarImage src={avatarSrc} alt={`${userName}'s avatar`} />
+      </Avatar>
+      <span className="text-sm">{userName}</span>
+    </Button>
+  );
+};
--- packages/ui/src/organisms/menu-top.tsx
+++ packages/ui/src/organisms/menu-top.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-import { Button } from '../atoms';
+import { ButtonProfile, ButtonProfileProps } from './button-profile';
 import { ButtonNavItem } from '../atoms/button-nav-item';
 
 type Dao = {
@@ -13,34 +12,37 @@ type MenuTopNavItem = {
 };
 
 type MenuTopProps = {
-  activeDao: Dao;
+  activeDao?: Dao;
   navItems: MenuTopNavItem[];
   createActionHref: string;
+  activeUser: ButtonProfileProps;
 };
 
 export const MenuTop = ({
-  activeDao: { title },
+  activeDao,
   navItems,
   createActionHref,
+  activeUser,
 }: MenuTopProps) => {
   console.debug('MenuTopProps', { createActionHref });
   return (
-    <div className="fixed top-0 right-0 flex items-center h-20 px-10 left-20 bg-background/5">
-      <div id="menu-top-active-dao">
-        <h1 className="text-2xl">{title}</h1>
+    <div className="fixed top-0 right-0 flex items-center space-x-10 h-20 px-10 left-20 bg-background/5">
+      <div id="menu-top-active-dao" className="flex-grow">
+        {activeDao && <h1 className="text-2xl">{activeDao.title}</h1>}
       </div>
       <div
         id="menu-top-actions"
-        className="flex justify-center flex-grow gap-2"
+        className="flex justify-center gap-2"
       >
         {navItems.map((item) => (
           <ButtonNavItem key={item.href} href={item.href} label={item.label} />
         ))}
       </div>
-      <div id="menu-top-create-action">
-        <Button asChild>
-          <Link href={createActionHref}>Create</Link>
-        </Button>
+      <div id="menu-top-profile">
+        <ButtonProfile
+          avatarSrc={activeUser.avatarSrc}
+          userName={activeUser.userName}
+        />
       </div>
     </div>
   );
