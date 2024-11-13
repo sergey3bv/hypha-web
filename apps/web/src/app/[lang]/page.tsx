a80f7b652cd28a765a616a846c993ebc14ca815a
Evgeni Hypha <evgeni.biryuk.hypha@gmail.com>
2024-11-13 15:37:53 +0300

    feat(DhosPage): added footer component into page

--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -5,6 +5,7 @@ import { Locale } from '@hypha-platform/i18n';
 import { getAssignmentsPath } from './dho/[id]/assignments/constants';
 import { Container } from '@hypha-platform/ui';
 import { Heading } from 'packages/ui/src/atoms/heading';
+import { Footer } from '@hypha-platform/ui/server';
 
 type PageProps = {
   params: { lang: Locale; id: string };
@@ -15,9 +16,9 @@ export default async function Index({ params: { lang } }: PageProps) {
   const daos = await getDaoList({ token: newtoken.accessJWT });
 
   return (
-    <div className="w-full px-6 py-4 overflow-auto">
+    <div className="w-full overflow-auto">
       <Container>
-        <Heading className="mb-4" size="8" color="secondary" weight="medium" align="center">All your spaces, in one place</Heading>
+        <Heading className="mb-4 mt-3" size="8" color="secondary" weight="medium" align="center">All your spaces, in one place</Heading>
         <div data-testid="dho-list-container" className="w-full">
           {daos.map((dao) => (
             <div key={dao.name} className="mb-5">
@@ -37,6 +38,7 @@ export default async function Index({ params: { lang } }: PageProps) {
           ))}
         </div>
       </Container>
+      <Footer/>
     </div>
   );
 }
