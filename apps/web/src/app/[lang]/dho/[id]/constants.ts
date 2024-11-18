5bd096975fd565ac74da2a495d33166ea9c46e7d
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-11-18 16:30:21 +0300

    fix(Layout): links to dho details page

--- apps/web/src/app/[lang]/dho/[id]/constants.ts
+++ apps/web/src/app/[lang]/dho/[id]/constants.ts
@@ -1,4 +1,9 @@
+import { Locale } from '@hypha-platform/i18n';
 import { PATH_LANG } from '../../constants';
 export const PATH_SEGMENT_DHO_ID = ':id';
 
 export const PATH_DHO = `${PATH_LANG}/dho/${PATH_SEGMENT_DHO_ID}`;
+
+export const getDhoPath = (lang: Locale, id: string) => {
+  return `/${lang}/dho/${id}`;
+};
