5cc62bd09d893fe3f7e2439a2ade0c88c1f1bd99
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-11-18 10:01:54 +0300

    feat(Structure): added app/[lang]/page.tsx

---
+++ apps/web/src/app/[lang]/page.tsx
@@ -0,0 +1,9 @@
+import { Locale } from '@hypha-platform/i18n';
+
+type PageProps = {
+  params: { lang: Locale; id: string };
+};
+
+export default async function Index({ params: { lang } }: PageProps) {
+  return (<div></div>)
+}
