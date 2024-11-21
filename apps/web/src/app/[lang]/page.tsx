9b23785ce49eafdbbdafc86c076a35439590e29a
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-11-21 11:44:01 +0300

    feat(Layout): added temp link to my-spaces

--- apps/web/src/app/[lang]/page.tsx
+++ apps/web/src/app/[lang]/page.tsx
@@ -1,9 +1,19 @@
 import { Locale } from '@hypha-platform/i18n';
+import Link from 'next/link';
+import { Button } from '@hypha-platform/ui';
 
 type PageProps = {
   params: { lang: Locale; id: string };
 };
 
 export default async function Index({ params: { lang } }: PageProps) {
-  return (<div>IndexPage</div>)
+  return (
+  <div>
+    <div>IndexPage</div>
+    <div>
+      <Button asChild variant="ghost" className="rounded-lg justify-start text-gray-400 px-0">
+        <Link href={`${lang}/my-spaces/`}>My Spaces</Link>
+      </Button>
+    </div>
+  </div>)
 }
