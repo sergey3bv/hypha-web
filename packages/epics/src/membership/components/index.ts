2a2e2658560ae35156f18342b3607514a945f2f5
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-11-25 15:31:52 +0300

    feat(ListMembers): added ListMembersComponent

--- packages/epics/src/membership/components/index.ts
+++ packages/epics/src/membership/components/index.ts
@@ -2,3 +2,5 @@ export * from './card-outer-space';
 export * from './list-outer-spaces';
 export * from './card-inner-space';
 export * from './list-inner-spaces';
+export * from './card-member';
+export * from './list-members';
