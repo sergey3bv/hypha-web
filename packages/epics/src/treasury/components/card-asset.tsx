9afb227799225402e2877e789e22af5dc02601ca
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-11-26 15:38:39 +0300

    feat(CardAsset): created new CardAsset component

---
+++ packages/epics/src/treasury/components/card-asset.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Text } from '@radix-ui/themes';
+import { Card } from '@hypha-platform/ui';
+import Image from 'next/image';
+
+type AssetCardProps = {
+  icon: string;
+  name: string;
+  symbol: string;
+  value: number;
+  usdEqual: number;
+  type:  string;
+};
+
+export const CardAsset: React.FC<AssetCardProps> = ({
+  icon,
+  name,
+  symbol,
+  value,
+  usdEqual,
+  type
+}) => {
+  const formatValue = (num:Number):String => {
+    let numStr = num.toString();
+    let [integerPart, decimalPart] = numStr.split('.');
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return decimalPart ? `${integerPart},${decimalPart}` : integerPart;
+  }
+  return (
+    <Card className='w-full h-full p-6 mb-2 flex flex-col justify-between'>
+      <div className='w-full flex flex-row items-center'>
+        <div className='mr-5'>
+          <Image src={icon} height={40} width={40} alt={name}></Image>
+        </div>
+        <div className='flex flex-col justify-center'>
+          <Text className='text-lg font-medium text-secondary-foreground'>{name}</Text>
+          <Text className='text-xs text-gray-500'>{symbol}</Text>
+        </div>
+      </div>
+      <div className='w-full flex flex-row'>
+          <Text className='text-secondary-foreground font-bold text-xs'>$ {formatValue(usdEqual)}</Text>
+          <Text className='text-gray-500 text-xs ml-1'>{formatValue(value)} {symbol}</Text>
+      </div>
+    </Card>
+  )
+}
+
