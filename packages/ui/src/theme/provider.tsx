0ef24cf83295a9513021536f5379449dc254000f
Philipp Litzenberger <philipp@bendcircular.com>
2024-12-19 15:01:22 +0100

    fix: build

--- packages/ui/src/theme/provider.tsx
+++ packages/ui/src/theme/provider.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import {
-  ThemeProvider as NextThemesProvider,
-  ThemeProviderProps,
-} from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProviderProps } from 'next-themes/dist/types';
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   children,
