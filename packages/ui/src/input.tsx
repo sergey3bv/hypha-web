4889b7b44714f475f2a57b9c0f3fa14d95515248
Philipp Litzenberger <philipp@bendcircular.com>
2024-12-19 18:00:12 +0100

    feat(FormComment #93): set correct input height

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "flex h-6 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             "placeholder:text-muted-foreground placeholder:text-2 placeholder:text-medium",
             leftIcon && "pl-12",
             rightIcon && "pr-12",
