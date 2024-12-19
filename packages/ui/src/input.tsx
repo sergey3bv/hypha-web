82eee94f3f821633841a18c502e335cb392bbdc2
Philipp Litzenberger <philipp@bendcircular.com>
2024-12-19 17:56:17 +0100

    feat(FormComment #93): init comment form

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -9,25 +9,26 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, leftIcon, rightIcon, ...props }, ref) => {
     return (
-      <div className="relative flex items-center">
+      <div className="relative flex items-center h-6">
         {leftIcon && (
-          <div className="absolute left-3 flex items-center pointer-events-none text-muted-foreground">
+          <div className="absolute left-2 flex items-center pointer-events-none text-muted-foreground">
             {leftIcon}
           </div>
         )}
         <input
           type={type}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            leftIcon && "pl-10",
-            rightIcon && "pr-10",
+            "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "placeholder:text-muted-foreground placeholder:text-2 placeholder:text-medium",
+            leftIcon && "pl-12",
+            rightIcon && "pr-12",
             className
           )}
           ref={ref}
           {...props}
         />
         {rightIcon && (
-          <div className="absolute right-3 flex items-center pointer-events-none text-muted-foreground">
+          <div className="absolute right-2 flex items-center pointer-events-none text-muted-foreground">
             {rightIcon}
           </div>
         )}
