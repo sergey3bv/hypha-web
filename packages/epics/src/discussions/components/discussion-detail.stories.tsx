64deaa88977e6720d942d06812d9c9892dabb457
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-12-25 05:48:15 -0800

    Update packages/epics/src/discussions/components/discussion-detail.stories.tsx
    Co-authored-by: Philipp Litzenberger <philipp@bendcircular.com>

--- packages/epics/src/discussions/components/discussion-detail.stories.tsx
+++ packages/epics/src/discussions/components/discussion-detail.stories.tsx
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 import { DiscussionDetail } from './discussion-detail';
 
 const meta: Meta<typeof DiscussionDetail> = {
