15417562dc8b89ccfc6a6b1c0f1904c23bf8b361
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2024-12-27 10:41:48 +0300

    feat(MemberCard): created storyfile form MemberCard

---
+++ packages/epics/src/membership/components/members/member-card.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MemberCard } from './member-card';
+
+const meta: Meta<typeof MemberCard> = {
+  component: MemberCard,
+  title: 'Epics/Membership/MemberCard',
+};
+export default meta;
+type Story = StoryObj<typeof MemberCard>;
+
+export const Primary: Story = {
+  args: {
+    avatar:
+      'https://images.unsplash.com/photo-1544005313-94ddf0286df2?&w=64&h=64&dpr=2&q=70&crop=faces&fit=crop',
+    name: 'Name',
+    surname: 'Surname',
+    nickname: 'username',
+    status: 'applicant',
+    commitment: 50,
+    location: 'Paris, France',
+    isLoading: false
+  },
+};
