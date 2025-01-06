085038201c5099c05feed6253c794a0e4639eb0c
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-06 21:39:19 +0100

    fix(#185): Layout Cleanup

--- packages/ui/src/layouts/html.tsx
+++ packages/ui/src/layouts/html.tsx
@@ -8,9 +8,9 @@ export const Html: React.FC<{
     <html
       lang="en"
       suppressHydrationWarning
-      className={clsx(className, 'fixed top-0 left-0 right-0 bottom-0')}
+      className={className}
     >
-      <body className="flex w-full h-full">{children}</body>
+      <body className="flex flex-col w-full h-full">{children}</body>
     </html>
   );
 };
