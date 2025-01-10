de04a63a4788a9416650b8d6a6bc83741e6876f1
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-10 11:27:35 +0100

    feat(#125) add markdown component

---
+++ packages/ui/src/molecules/markdown/markdown.mocks.ts
@@ -0,0 +1,35 @@
+export const content = `
+# Hello, world!
+
+This is a test of the markdown component.
+
+---
+
+## This is a subheading
+
+This is a paragraph. This is a [link](https://www.google.com).
+
+### This is a subsubheading
+
+This is a paragraph.
+
+#### This is a subsubsubheading
+
+This is a paragraph.
+
+- This is a list item.
+- This is a list item.
+- This is a list item.
+
+1. This is a list item.
+2. This is a list item.
+3. This is a list item.
+
+**This is a bold text.**
+
+*This is an italic text.*
+
+
+> This is a blockquote.
+
+`;
---
+++ packages/ui/src/molecules/markdown/prose-wrapper.tsx
@@ -0,0 +1,3 @@
+export const ProseWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div className="prose">{children}</div>;
+};
