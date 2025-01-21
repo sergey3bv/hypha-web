f68d22ce305a5944e2222f27a285cc4fb5debca2
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-01-21 14:21:51 +0300

    fix(AgreementsList): fixed props

--- packages/epics/src/agreements/components/agreements-list.tsx
+++ packages/epics/src/agreements/components/agreements-list.tsx
@@ -2,13 +2,11 @@ import { FC } from 'react';
 import { AgreementCard } from './agreement-card';
 import { useAgreements } from '../hooks/use-agreements';
 import Link from 'next/link';
-import { AgreementItem } from '@hypha-platform/graphql/rsc';
 
 type AgreementsListProps = {
   page: number;
   activeFilter: string;
   basePath: string;
-  agreementsProp?: AgreementItem[];
   hasAvatar?: boolean;
 };
 
@@ -16,7 +14,6 @@ export const AgreementsList: FC<AgreementsListProps> = ({
   page,
   activeFilter,
   basePath,
-  agreementsProp,
   hasAvatar,
 }) => {
   const { agreements, isLoading } = useAgreements({
@@ -25,22 +22,16 @@ export const AgreementsList: FC<AgreementsListProps> = ({
   });
   return (
     <div className="agreement-list w-full">
-      {(agreementsProp ? agreementsProp : agreements).map(
-        (agreement, index) => (
-          <Link
-            href={`${basePath}/${agreement.slug}`}
+      {agreements.map((agreement, index) => (
+        <Link href={`${basePath}/${agreement.slug}`} key={index} scroll={false}>
+          <AgreementCard
+            hasAvatar={hasAvatar}
             key={index}
-            scroll={false}
-          >
-            <AgreementCard
-              hasAvatar={hasAvatar}
-              key={index}
-              {...agreement}
-              isLoading={isLoading}
-            />
-          </Link>
-        ),
-      )}
+            {...agreement}
+            isLoading={isLoading}
+          />
+        </Link>
+      ))}
       {isLoading ? (
         <div>
           <AgreementCard isLoading={isLoading} />
