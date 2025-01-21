f391ef638e1df70d5bb759d7f0e4a3b3578694aa
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-21 18:44:27 +0100

    feat(#209): format

--- packages/epics/src/profile/hooks/use-profile.ts
+++ packages/epics/src/profile/hooks/use-profile.ts
@@ -12,7 +12,7 @@ export const useProfile = ({ address }: { address?: string }) => {
         avatar:
           'https://images.unsplash.com/photo-1544005313-94ddf0286df2?&w=64&h=64&dpr=2&q=70&crop=faces&fit=crop',
       };
-    }
+    },
   );
 
   return { profile, isLoading };
