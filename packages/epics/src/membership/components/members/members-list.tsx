5f33c529ab2f083e59865d5a5c85346333607490
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-01-21 15:02:50 +0300

    feat(MemberList): removed duplicate prop

--- packages/epics/src/membership/components/members/members-list.tsx
+++ packages/epics/src/membership/components/members/members-list.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
 import { MemberCard } from './member-card';
 import { useMembers } from '../../hooks/use-members';
-import { MemberCardProps } from './member-card';
 
 type MembersListProps = {
   page: number;
   activeFilter: string;
-  membersProp?: MemberCardProps[];
   isLoadingProp?: boolean;
   minimize?: boolean;
 };
@@ -14,7 +12,6 @@ type MembersListProps = {
 export const MembersList: FC<MembersListProps> = ({
   page,
   activeFilter,
-  membersProp,
   isLoadingProp,
   minimize,
 }) => {
@@ -24,7 +21,7 @@ export const MembersList: FC<MembersListProps> = ({
   });
   return (
     <div className="member-list w-full">
-      {(membersProp ? membersProp : members).map((member, index) => (
+      {members.map((member, index) => (
         <MemberCard
           minimize={minimize}
           key={index}
