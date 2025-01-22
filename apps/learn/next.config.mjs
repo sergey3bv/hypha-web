75068cbea369af7ee1511c7e21710993a5eca5f0
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-22 10:51:08 +0100

    feat(search): project configuration

--- apps/learn/next.config.mjs
+++ apps/learn/next.config.mjs
@@ -1,13 +1,58 @@
-import nextra from 'nextra'
+//@ts-check
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import { composePlugins, withNx } from '@nx/next';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import nextra from 'nextra';
+
+// @ts-ignore
 const withNextra = nextra({
   latex: true,
   search: {
-    codeblocks: false
+    codeblocks: false,
   },
-  contentDirBasePath: '/docs'
-})
+  contentDirBasePath: '/docs',
+});
 
-export default withNextra({
-  reactStrictMode: true
-})
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hypha.infura-ipfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  withNextra,
+];
+
+export default composePlugins(...plugins)(nextConfig);
