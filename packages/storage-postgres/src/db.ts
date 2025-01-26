700a9d48577ee76c64817f4bee5547ab821bfd83
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-26 12:11:14 +0100

    feat(#255): Swappable Data Layer
    - log postgres url

--- packages/storage-postgres/src/db.ts
+++ packages/storage-postgres/src/db.ts
@@ -1,9 +1,14 @@
 // src/db.ts
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
-import { config } from 'dotenv';
 
-config({ path: '../.env' }); // or .env.local
+const url = process.env.POSTGRES_URL;
 
-const sql = neon(process.env.POSTGRES_URL!);
+console.debug('POSTGRES_URL', url);
+
+if (!url) {
+  throw new Error('POSTGRES_URL is not set');
+}
+
+const sql = neon(url);
 export const db = drizzle({ client: sql });
