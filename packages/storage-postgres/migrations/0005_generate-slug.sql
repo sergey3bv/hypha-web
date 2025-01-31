23104cafca84470f40133150439cda54e3983b2e
Philipp Litzenberger <philipp@bendcircular.com>
2025-01-31 11:27:22 +0100

    feat(#287): Governance Entities Interface
    - update generate_slug

--- packages/storage-postgres/migrations/0005_generate-slug.sql
+++ packages/storage-postgres/migrations/0005_generate-slug.sql
@@ -2,12 +2,19 @@
 CREATE OR REPLACE FUNCTION generate_slug()
 RETURNS TRIGGER AS $$
 BEGIN
-  NEW.slug := lower(replace(NEW.title, ' ', '-'));
+  IF NEW.slug IS NULL THEN
+    NEW.slug := lower(replace(NEW.title, ' ', '-'));
+  END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER before_insert_documents
 BEFORE INSERT ON documents
+FOR EACH ROW
+EXECUTE FUNCTION generate_slug();
+
+CREATE TRIGGER before_update_documents
+BEFORE UPDATE ON documents
 FOR EACH ROW
 EXECUTE FUNCTION generate_slug();
