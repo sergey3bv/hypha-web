cdc3d9195c6025bb6b657326d5a8204553e2f350
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-10 09:03:32 +0100

    feat(#383): combine schema export

--- packages/storage-postgres/src/schema/index.ts
+++ packages/storage-postgres/src/schema/index.ts
@@ -1,5 +1,14 @@
-export * from './document';
+import { memberships } from './membership';
+import { people } from './people';
+import { spaces } from './space';
+import { spaceConfigs } from './space-config';
 export * from './membership';
 export * from './people';
 export * from './space-config';
 export * from './space';
+export const schema = {
+  memberships,
+  people,
+  spaceConfigs,
+  spaces,
+};
