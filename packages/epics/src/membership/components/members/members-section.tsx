a341f7fd7658a984551d8cfc90b344a47f638d06
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-11 09:56:27 +0300

    feat(MembersSection): added empty state

--- packages/epics/src/membership/components/members/members-section.tsx
+++ packages/epics/src/membership/components/members/members-section.tsx
@@ -42,30 +42,38 @@ export const MembersSection: FC<MemberSectionProps> = ({ basePath }) => {
           Invite member
         </Button>
       </SectionFilter>
-      <SectionTabs
-        activeTab={activeFilter}
-        setActiveTab={setActiveFilter}
-        tabs={filterOptions}
-      />
-      {Array.from({ length: pages }).map((_, index) => (
-        <MembersList
-          basePath={basePath}
-          page={index + 1}
-          key={index}
-          activeFilter={activeFilter}
+      {members?.length === 0 ? null : (
+        <SectionTabs
+          activeTab={activeFilter}
+          setActiveTab={setActiveFilter}
+          tabs={filterOptions}
         />
-      ))}
-      <SectionLoadMore
-        onClick={loadMore}
-        disabled={pagination?.totalPages === pages}
-        isLoading={isLoading}
-      >
-        <Text>
-          {pagination?.totalPages === pages
-            ? 'No more members'
-            : 'Load more members'}
-        </Text>
-      </SectionLoadMore>
+      )}
+      {members?.length === 0 ? (
+        <Text className="text-neutra-11 mt-2 mb-6">List is empty</Text>
+      ) : (
+        Array.from({ length: pages }).map((_, index) => (
+          <MembersList
+            basePath={basePath}
+            page={index + 1}
+            key={index}
+            activeFilter={activeFilter}
+          />
+        ))
+      )}
+      {members?.length === 0 ? null : (
+        <SectionLoadMore
+          onClick={loadMore}
+          disabled={pagination?.totalPages === pages}
+          isLoading={isLoading}
+        >
+          <Text>
+            {pagination?.totalPages === pages
+              ? 'No more members'
+              : 'Load more members'}
+          </Text>
+        </SectionLoadMore>
+      )}
     </div>
   );
 };
