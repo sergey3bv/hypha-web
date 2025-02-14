cb777b1fd015bf2f0b51630cf3aea7ddec7b00da
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-14 15:16:38 +0100

    feat(#380): remove duplicate component

--- packages/epics/src/profile/components/person-avatar.tsx
+++ packages/epics/src/profile/components/person-avatar.tsx
@@ -19,8 +19,6 @@ export const PersonAvatar = ({
     } else if (nameParts.length >= 2) {
       return `${nameParts[0].charAt(0)}${nameParts[1].charAt(0)}`;
     }
-
-    return <UserIcon className="w-4 h-4" />;
   };
 
   return (
