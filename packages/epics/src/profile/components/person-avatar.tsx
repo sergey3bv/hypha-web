74c6f717acba1f0c4af1ddf8a962c650ea116cc3
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-14 12:18:55 +0300

    feat(#380): design fallback avatar

--- packages/epics/src/profile/components/person-avatar.tsx
+++ packages/epics/src/profile/components/person-avatar.tsx
@@ -1,4 +1,5 @@
-import { Avatar, AvatarImage } from '@hypha-platform/ui';
+import { Avatar, AvatarImage, AvatarFallback } from '@hypha-platform/ui';
+import { UserIcon } from 'lucide-react';
 
 export const PersonAvatar = ({
   avatarSrc,
@@ -7,9 +8,25 @@ export const PersonAvatar = ({
   avatarSrc?: string;
   userName?: string;
 }) => {
+  const getFallbackContent = () => {
+    if (!userName) {
+      return <UserIcon className="w-4 h-4" />;
+    }
+
+    const nameParts = userName.split(' ');
+    if (nameParts.length === 1) {
+      return nameParts[0].charAt(0);
+    } else if (nameParts.length >= 2) {
+      return `${nameParts[0].charAt(0)}${nameParts[1].charAt(0)}`;
+    }
+
+    return <UserIcon className="w-4 h-4" />;
+  };
+
   return (
     <Avatar className="w-7 h-7 rounded-lg">
       <AvatarImage src={avatarSrc} alt={`${userName}'s avatar`} />
+      <AvatarFallback>{getFallbackContent()}</AvatarFallback>
     </Avatar>
   );
 };
