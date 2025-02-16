cb6cccb297ccd3b7ef4601ec5420f347c7888482
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-16 13:16:46 +0100

    feat(#420): add useMembers mock to storybook

--- packages/epics/src/membership/components/members/members-section.stories.tsx
+++ packages/epics/src/membership/components/members/members-section.stories.tsx
@@ -11,7 +11,41 @@ export default meta;
 type Story = StoryObj<typeof MembersSection>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    useMembers: () => ({
+      isLoading: false,
+      pagination: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+        page: 1,
+        pageSize: 10,
+        total: 2,
+        totalPages: 1,
+      },
+      members: [
+        {
+          id: 1,
+          avatarUrl:
+            'https://images.unsplash.com/photo-1544005313-94ddf0286df2?&w=64&h=64&dpr=2&q=70&crop=faces&fit=crop',
+          name: 'Jane',
+          surname: 'Doe',
+          nickname: 'username',
+          location: 'Paris, France',
+          slug: 'member-1',
+        },
+        {
+          id: 2,
+          avatarUrl:
+            'https://images.unsplash.com/photo-1544005313-94ddf0286df2?&w=64&h=64&dpr=2&q=70&crop=faces&fit=crop',
+          name: 'Jon',
+          surname: 'Doe',
+          nickname: 'username',
+          location: 'Paris, France',
+          slug: 'member-2',
+        },
+      ],
+    }),
+  },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     expect(canvas.getByText(/Members \|/gi)).toBeTruthy();
