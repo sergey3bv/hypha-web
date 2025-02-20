b927f10e858b28a5dbd7e7c2c03712d90547cef9
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-20 13:32:27 +0300

    feat(#64): added space type ui in CreateForm

--- packages/epics/src/common/create-form/create-form.tsx
+++ packages/epics/src/common/create-form/create-form.tsx
@@ -1,7 +1,18 @@
+'use client';
 import { CreateFormHead, CreateFormHeadProps } from './create-form-head';
-import { Button, Skeleton, FileUploader, Textarea } from '@hypha-platform/ui';
+import {
+  Button,
+  Skeleton,
+  FileUploader,
+  Textarea,
+  Input,
+  Switch,
+} from '@hypha-platform/ui';
 import { RxCross1 } from 'react-icons/rx';
 import { SelectItem, SelectMenu } from '@hypha-platform/ui/server';
+import { useState } from 'react';
+import { Text } from '@radix-ui/themes';
+import { cn } from '@hypha-platform/lib/utils';
 
 import Link from 'next/link';
 import React from 'react';
@@ -17,6 +28,22 @@ export const CreateForm = ({
   type,
 }: CreateFormProps) => {
   const [files, setFiles] = React.useState<File[]>([]);
+
+  const isSpaceForm = type?.toLowerCase() === 'space';
+
+  const [activeLinks, setActiveLinks] = useState({
+    website: false,
+    linkedin: false,
+    x: false,
+  });
+
+  const handleLinkToggle = React.useCallback(
+    (field: keyof typeof activeLinks) => (isActive: boolean) => {
+      setActiveLinks({ ...activeLinks, [field]: isActive });
+    },
+    [activeLinks, setActiveLinks],
+  );
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex gap-5 justify-between">
@@ -52,26 +79,104 @@ export const CreateForm = ({
       >
         <Textarea placeholder="Type a brief description here..." />
       </Skeleton>
-      <div className="row mt-1 flex justify-between w-full">
-        <Skeleton
-          width="62px"
-          height="20px"
-          loading={isLoading}
-          className="rounded-lg"
-        >
-          <span className="text-2 text-neutral-11 font-medium">Outcome</span>
-        </Skeleton>
-        <Skeleton
-          width="62px"
-          height="32px"
-          loading={isLoading}
-          className="rounded-lg"
-        >
-          <SelectMenu variant="default" value={'select-one'}>
-            <SelectItem value="select-one">Select one</SelectItem>
-          </SelectMenu>
-        </Skeleton>
-      </div>
+      {isSpaceForm ? null : (
+        <div className="row mt-1 flex justify-between w-full">
+          <Skeleton
+            width="62px"
+            height="20px"
+            loading={isLoading}
+            className="rounded-lg"
+          >
+            <span className="text-2 text-neutral-11 font-medium">Outcome</span>
+          </Skeleton>
+          <Skeleton
+            width="62px"
+            height="32px"
+            loading={isLoading}
+            className="rounded-lg"
+          >
+            <SelectMenu variant="default" value={'select-one'}>
+              <SelectItem value="select-one">Select one</SelectItem>
+            </SelectMenu>
+          </Skeleton>
+        </div>
+      )}
+      {isSpaceForm ? (
+        <div className="flex gap-6 flex-col">
+          <div className="flex justify-between">
+            <Text
+              className={cn(
+                'text-2',
+                activeLinks.website ? 'text-neutral-11' : 'text-neutral-8',
+              )}
+            >
+              Website
+            </Text>
+            <span className="flex items-center">
+              <Input
+                placeholder="Add your URL"
+                disabled={!activeLinks.website}
+                className={cn(
+                  'text-2 mr-3',
+                  !activeLinks.website ? 'bg-neutral-6' : '',
+                )}
+              />
+              <Switch
+                checked={activeLinks.website}
+                onCheckedChange={handleLinkToggle('website')}
+              />
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <Text
+              className={cn(
+                'text-2',
+                activeLinks.website ? 'text-neutral-11' : 'text-neutral-8',
+              )}
+            >
+              LinkedIn
+            </Text>
+            <span className="flex items-center">
+              <Input
+                placeholder="Add your URL"
+                disabled={!activeLinks.linkedin}
+                className={cn(
+                  'text-2 mr-3',
+                  !activeLinks.linkedin ? 'bg-neutral-6' : '',
+                )}
+              />
+              <Switch
+                checked={activeLinks.linkedin}
+                onCheckedChange={handleLinkToggle('linkedin')}
+              />
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <Text
+              className={cn(
+                'text-2',
+                activeLinks.x ? 'text-neutral-11' : 'text-neutral-8',
+              )}
+            >
+              X
+            </Text>
+            <span className="flex items-center">
+              <Input
+                placeholder="Add your URL"
+                disabled={!activeLinks.x}
+                className={cn(
+                  'text-2 mr-3',
+                  !activeLinks.x ? 'bg-neutral-6' : '',
+                )}
+              />
+              <Switch
+                checked={activeLinks.x}
+                onCheckedChange={handleLinkToggle('x')}
+              />
+            </span>
+          </div>
+        </div>
+      ) : null}
       <div className="flex justify-end w-full">
         <Skeleton
           width="72px"
@@ -83,7 +188,7 @@ export const CreateForm = ({
             variant="default"
             className="rounded-lg justify-start text-white w-fit"
           >
-            Publish
+            {isSpaceForm ? 'Create' : 'Publish'}
           </Button>
         </Skeleton>
       </div>
