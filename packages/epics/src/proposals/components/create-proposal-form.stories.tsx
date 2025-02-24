5e44c32df16a2a0bb12762baabcd4eb7eb12a628
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-02-24 15:56:25 +0300

    feat(#83): Added create proposal form component

---
+++ packages/epics/src/proposals/components/create-proposal-form.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CreateProposalForm } from './create-proposal-form';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta = {
+  component: CreateProposalForm,
+  title: 'Epics/Proposals/CreateProposalForm',
+} satisfies Meta<typeof CreateProposalForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    creator: {
+      avatar: 'https://github.com/shadcn.png',
+      name: 'Name',
+      surname: 'Surname',
+    },
+    isLoading: false,
+    closeUrl: '',
+    outcomeOptions: [
+      {
+        label: 'Payment',
+        value: 'payment',
+      },
+    ],
+    numberOfWeekOptions: [
+      {
+        label: '4',
+        value: '4',
+      },
+    ],
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Outcome!/gi)).toBeTruthy();
+  },
+};
