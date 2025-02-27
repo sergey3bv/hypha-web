1301514fa15965426d8015e54ecb288d78f1ad41
Philipp Litzenberger <philipp@bendcircular.com>
2025-02-27 11:17:24 +0100

    feat(#405): add drizzle db setup and migrate people table

--- apps/web/src/app/api/v1/people/me/route.ts
+++ apps/web/src/app/api/v1/people/me/route.ts
@@ -1,10 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { neon } from '@neondatabase/serverless';
+import { people } from '@hypha-platform/storage-postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+import { eq, sql } from 'drizzle-orm';
 
 export async function GET(request: NextRequest) {
-  const sql = neon(process.env.DEFAULT_DB_AUTHENTICATED_URL!, {
-    authToken: request.headers.get('Authorization')?.split(' ')[1] || '',
-  });
-  const user = await sql`SELECT * FROM auth.user_id()`;
+  const authToken = request.headers.get('Authorization')?.split(' ')[1] || '';
+
+  const db = drizzle(
+    neon(process.env.DEFAULT_DB_AUTHENTICATED_URL!, {
+      authToken: authToken, // Pass the generated JWT here
+    }),
+  );
+
+  const [user] = await db
+    .select()
+    .from(people)
+    .where(sql`auth.user_id() = ${people.sub}`)
+    .limit(1);
+
   return NextResponse.json({ user });
 }
--- packages/storage-postgres/src/schema/people.ts
+++ packages/storage-postgres/src/schema/people.ts
@@ -4,6 +4,7 @@ import { commonDateFields } from './shared';
 
 export const people = pgTable('people', {
   id: serial('id').primaryKey(),
+  sub: text('sub').unique(),
   slug: text('slug').unique(),
   avatarUrl: text('avatar_url'),
   description: text('description'),
