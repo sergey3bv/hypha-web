0a1ea2caea179401acced7cb7779cda45b2c878f
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-06 10:42:13 +0300

    feat(#426): added favicon like link tag

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="https://hypha.earth/wp-content/uploads/2023/07/cropped-favicon-32x32.png" sizes="32x32"/>
+        <link rel="icon" href="https://hypha.earth/wp-content/uploads/2023/07/cropped-favicon-192x192.png" sizes="192x192"/>
+      </head>
       <body>{children}</body>
     </html>
   );
