ee94b414cd9417e455a8cba5b53e63fb7c43a746
Philipp Litzenberger <philipp@bendcircular.com>
2025-03-10 10:41:13 +0100

    feat(#573): Remove duplicate layout

--- packages/ui/src/layouts/html.tsx
+++ packages/ui/src/layouts/html.tsx
@@ -4,6 +4,18 @@ export const Html: React.FC<{
 }> = ({ children, className }) => {
   return (
     <html lang="en" suppressHydrationWarning className={className}>
+      <head>
+        <link
+          rel="icon"
+          href="https://hypha.earth/wp-content/uploads/2023/07/cropped-favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          href="https://hypha.earth/wp-content/uploads/2023/07/cropped-favicon-192x192.png"
+          sizes="192x192"
+        />
+      </head>
       <body className="flex flex-col w-full h-full">{children}</body>
     </html>
   );
