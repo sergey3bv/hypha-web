b3592e51e98076971dd5cbb9559877af8c9bc24f
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-12 16:41:58 +0300

    feat(#587): created documents stats component

---
+++ packages/epics/src/governance/components/document-stats.tsx
@@ -0,0 +1,31 @@
+import { Skeleton } from '@hypha-platform/ui';
+import { EyeOpenIcon, ChatBubbleIcon } from '@radix-ui/react-icons';
+
+interface DocumentStatsProps {
+  isLoading?: boolean;
+  views?: number;
+  comments?: number;
+}
+
+export const DocumentStats = ({
+  isLoading,
+  views,
+  comments,
+}: DocumentStatsProps) => {
+  return (
+    <div className="flex flex-grow gap-2 text-1 text-gray-500 items-center">
+      <Skeleton width="16px" height="16px" loading={isLoading}>
+        <div className="flex">
+          <EyeOpenIcon className="mr-1" width={16} />
+          <div>{views}</div>
+        </div>
+      </Skeleton>
+      <Skeleton width="16px" height="16px" loading={isLoading}>
+        <div className="flex ml-3">
+          <ChatBubbleIcon className="mr-1" width={16} />
+          <div>{comments}</div>
+        </div>
+      </Skeleton>
+    </div>
+  );
+};
