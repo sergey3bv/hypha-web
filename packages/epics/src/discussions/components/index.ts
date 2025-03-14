356b663942abd584f9209fc485e23e8f934d3c36
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-14 16:57:22 +0300

    feat(#595) removed unused exports

--- packages/epics/src/discussions/components/index.ts
+++ packages/epics/src/discussions/components/index.ts
@@ -1,6 +1,4 @@
 export * from './discussion-card';
 export * from './discussion-list';
 export * from './discussions-section';
-export * from '../../interactions/components/chat';
 export * from './discussion-detail';
-export * from '../../interactions/components/message';
