e41dc1cee947a8f67866fb88089b7eecf7e22168
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-17 11:13:41 +0300

    feat(#595): created document-grid component with storybook file

---
+++ packages/epics/src/governance/components/document-grid.tsx
@@ -0,0 +1,42 @@
+import { type BadgeItem } from '@hypha-platform/ui';
+import { type Creator } from '../../people/components/person-label';
+import { DocumentCard } from './document-card';
+import Link from 'next/link';
+
+interface Document {
+  title?: string;
+  description?: string;
+  leadImage?: string;
+  creator?: Creator;
+  badges?: BadgeItem[];
+  slug?: string;
+  interactions?: React.ReactNode;
+}
+
+interface DocumentGridProps {
+  isLoading: boolean;
+  basePath: string;
+  documents: Document[];
+}
+
+export const DocumentGrid = ({
+  isLoading = true,
+  basePath,
+  documents,
+}: DocumentGridProps) => {
+  return (
+    <div className="w-full">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-3 space-x-2">
+        {documents.map((document) => (
+          <Link
+            href={`${basePath}/${document.slug}`}
+            key={document.slug}
+            scroll={false}
+          >
+            <DocumentCard {...document} isLoading={isLoading} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
