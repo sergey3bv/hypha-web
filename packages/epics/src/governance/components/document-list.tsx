0667e7b38d25c454362704349ba44f711cf319e5
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-17 11:13:15 +0300

    feat(#595): refactor document-list and created story file

--- packages/epics/src/governance/components/document-list.tsx
+++ packages/epics/src/governance/components/document-list.tsx
@@ -1,81 +1,40 @@
-import { DocumentCard } from './document-card';
 import { DocumentListCard } from './document-list-card';
-import { UseDocuments } from '../../governance';
+import { type BadgeItem } from '@hypha-platform/ui';
+import { type Creator } from '../../people/components/person-label';
 import Link from 'next/link';
-import { cn } from '@hypha-platform/lib/utils';
+
+interface Document {
+  title?: string;
+  description?: string;
+  leadImage?: string;
+  creator?: Creator;
+  badges?: BadgeItem[];
+  slug?: string;
+  interactions?: React.ReactNode;
+}
 
 interface DocumentListProps {
-  page: number;
+  isLoading: boolean;
   basePath: string;
-  useDocuments: UseDocuments;
-  hasGridView: boolean;
-}
-
-interface GridLoadersProps {
-  isLoading: boolean;
-}
-
-interface ListLoadersProps {
-  isLoading: boolean;
+  documents: Document[];
 }
 
 export const DocumentList = ({
-  page,
+  isLoading = true,
   basePath,
-  useDocuments,
-  hasGridView,
+  documents,
 }: DocumentListProps) => {
-  const { documents, isLoading } = useDocuments({
-    page,
-    filter: { state: '' },
-  });
-
-  const GridLoaders = ({ isLoading }: GridLoadersProps) => {
-    return isLoading ? (
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 mt-2">
-        <DocumentCard isLoading={isLoading} />
-        <DocumentCard isLoading={isLoading} />
-        <DocumentCard isLoading={isLoading} />
-      </div>
-    ) : null;
-  };
-
-  const ListLoaders = ({ isLoading }: ListLoadersProps) => {
-    return isLoading ? (
-      <div>
-        <DocumentListCard isLoading={isLoading} />
-        <DocumentListCard isLoading={isLoading} />
-        <DocumentListCard isLoading={isLoading} />
-      </div>
-    ) : null;
-  };
-
   return (
-    <div className="w-full">
-      <div
-        className={cn(
-          hasGridView ? 'grid grid-cols-1 sm:grid-cols-3 gap-2 mt-2' : '',
-        )}
-      >
-        {documents.map((document) => (
-          <Link
-            href={`${basePath}/${document.slug}`}
-            key={document.slug}
-            scroll={false}
-          >
-            {hasGridView ? (
-              <DocumentCard {...document} isLoading={isLoading} />
-            ) : (
-              <DocumentListCard {...document} isLoading={isLoading} />
-            )}
-          </Link>
-        ))}
-      </div>
-      {hasGridView ? (
-        <GridLoaders isLoading={isLoading} />
-      ) : (
-        <ListLoaders isLoading={isLoading} />
-      )}
+    <div className="w-full space-y-2 flex flex-col">
+      {documents.map((document) => (
+        <Link
+          href={`${basePath}/${document.slug}`}
+          key={document.slug}
+          scroll={false}
+        >
+          <DocumentListCard {...document} isLoading={isLoading} />
+        </Link>
+      ))}
     </div>
   );
 };
