a5a6520fbf2a4e7199690adc6ee01e0d164fd827
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-24 15:52:28 +0300

    feat(#628): created document-like-button component

---
+++ packages/epics/src/governance/components/document-like-button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DocumentLikeButton } from './document-like-button';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta = {
+  component: DocumentLikeButton,
+} satisfies Meta<typeof DocumentLikeButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof DocumentLikeButton>;
+
+export const Default: Story = {
+  args: {
+    isLiked: false,
+    isLoading: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Like/gi)).toBeTruthy();
+  },
+};
