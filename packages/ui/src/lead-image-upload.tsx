a97f5bc1002605e1f15848169fb59b11bde6bc9f
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-25 17:04:24 +0300

    feat(#633): created lead image upload

---
+++ packages/ui/src/lead-image-upload.tsx
@@ -0,0 +1,7 @@
+export const LeadImageUpload = () => {
+  return (
+    <div className="border border-neutral-11 border-dashed rounded-lg w-full h-[150px] flex justify-center items-center">
+      <span>Drop the file here</span>
+    </div>
+  );
+};
