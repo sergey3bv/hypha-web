cd49102f2ba2676919eca00a4fd5e511463b8ab2
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-26 10:14:16 +0300

    feat(#633): added ux for lead image upload

--- packages/ui/src/lead-image-upload.tsx
+++ packages/ui/src/lead-image-upload.tsx
@@ -1,7 +1,51 @@
-export const LeadImageUpload = () => {
+import { useDropzone } from 'react-dropzone';
+import React from 'react';
+export const LeadImageUpload = ({
+  onChange,
+}: {
+  onChange: (acceptedFile: File) => void;
+}) => {
+  const [preview, setPreview] = React.useState<string | null>('');
+
+  const onDrop = React.useCallback((acceptedFiles: File[]) => {
+    const reader = new FileReader();
+    try {
+      reader.onload = () => {
+        setPreview(reader.result as string);
+        onChange(acceptedFiles[0]);
+      };
+      reader.readAsDataURL(acceptedFiles[0]);
+    } catch (error) {
+      setPreview(null);
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    maxFiles: 1,
+    accept: { 'image/png': [], 'image/jpg': [], 'image/jpeg': [] },
+  });
+
   return (
-    <div className="border border-neutral-11 border-dashed rounded-lg w-full h-[150px] flex justify-center items-center">
-      <span>Drop the file here</span>
+    <div
+      {...getRootProps()}
+      className="border border-neutral-11 border-dashed rounded-lg w-full h-[150px] flex justify-center items-center"
+    >
+      <input {...getInputProps()} />
+      {preview && (
+        <img
+          className="w-full h-full object-cover"
+          src={preview}
+          alt="Preview"
+        />
+      )}
+      {isDragActive ? (
+        <span className={`${preview ? 'hidden' : ''}`}>Drop the file here</span>
+      ) : (
+        <span className={`${preview ? 'hidden' : ''}`}>
+          <span className="text-accent-11">Upload</span> an image
+        </span>
+      )}
     </div>
   );
 };
