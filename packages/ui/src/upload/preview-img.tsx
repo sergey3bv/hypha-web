18e38727bc66d1aacfb5d15098c23f0bf7ff1c6e
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-03-26 13:26:31 +0300

    feat(#633): implemented aspect-ration in preview-img

--- packages/ui/src/upload/preview-img.tsx
+++ packages/ui/src/upload/preview-img.tsx
@@ -1,6 +1,13 @@
+import { AspectRatio } from '../aspect-ratio';
+
 export type PreviewImgProps = {
   src: string;
+  aspectRatio?: number;
 };
-export const PreviewImg = ({ src }: PreviewImgProps) => {
-  return <img src={src} alt="Preview" className="w-full h-full object-cover" />;
+export const PreviewImg = ({ src, aspectRatio }: PreviewImgProps) => {
+  return (
+    <AspectRatio ratio={aspectRatio}>
+      <img src={src} alt="Preview" className="w-full h-full object-cover" />
+    </AspectRatio>
+  );
 };
