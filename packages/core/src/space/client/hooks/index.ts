7fba39d1f8f8baf1bb330e0b0aa812d84fcc747a
Philipp Litzenberger <philipp@bendcircular.com>
2025-03-27 06:47:36 +0100

    feat(#558): remove stale useSpaceMutationsWeb2Rest hook

--- packages/core/src/space/client/hooks/index.ts
+++ packages/core/src/space/client/hooks/index.ts
@@ -1,3 +1,2 @@
-export { useSpaceMutationsWeb2Rest } from './useSpaceMutations.web2.rest';
 export { useSpaceMutationsWeb2Rsc } from './useSpaceMutations.web2.rsc';
 export { useCreateSpaceOrchestrator } from './useCreateSpaceOrchestrator';
