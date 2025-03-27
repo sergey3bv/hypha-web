4ba17fb42ec76b1d4e587c25ef2c7a3f9455b50d
Philipp Litzenberger <philipp@bendcircular.com>
2025-03-27 06:47:34 +0100

    feat(#558): add ticket

--- packages/core-client/src/web3/provider.tsx
+++ packages/core-client/src/web3/provider.tsx
@@ -7,6 +7,7 @@ import { config } from './config';
 export const EvmProvider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
   return (
+    // TODO: #590 When we stick with wagmi, we should replace `useSWR` with `reactQuery`
     <QueryClientProvider client={queryClient}>
       <WagmiProvider config={config}>{children}</WagmiProvider>
     </QueryClientProvider>
