5c69bd30c01fbdaa96af3610679e5f6254061bac
Philipp Litzenberger <philipp@bendcircular.com>
2025-03-27 06:47:33 +0100

    feat(#558): separate client and server packages

--- packages/core/wagmi.config.ts
+++ packages/core/wagmi.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from '@wagmi/cli';
 import { hardhat } from '@wagmi/cli/plugins';
 
 export default defineConfig({
-  out: 'src/components/generated.ts',
+  out: 'src/generated.ts',
   contracts: [],
   plugins: [
     hardhat({
