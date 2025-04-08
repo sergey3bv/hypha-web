c25d8af9913deda073e878dd7f8c923f4a66cdd4
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-08 18:18:52 +0300

    feat(#623): fixed profile proposals state

--- apps/web/src/app/[lang]/profile/page.tsx
+++ apps/web/src/app/[lang]/profile/page.tsx
@@ -86,7 +86,7 @@ export default function Profile() {
           <DocumentSection
             basePath=""
             useDocuments={useSpaceDocuments}
-            documentState="completed"
+            documentState="proposal"
           />
         </TabsContent>
       </Tabs>
