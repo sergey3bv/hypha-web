7e8fbabdf45cf9e2406e7aa58fbd8cc4abf5de66
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-10 15:06:02 +0300

    Update apps/web/src/app/[lang]/dho/[id]/@aside/(.)treasury/deposit/loading.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/web/src/app/[lang]/dho/[id]/@aside/(.)treasury/deposit/loading.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/(.)treasury/deposit/loading.tsx
@@ -1,7 +1,7 @@
 import { DepositFunds } from '@hypha-platform/epics';
 import { SidePanel } from '../../_components/side-panel';
 
-export default async function Treasury({}) {
+export default async function Treasury() {
   return (
     <SidePanel>
       <DepositFunds closeUrl={''} spaceId={0} />
