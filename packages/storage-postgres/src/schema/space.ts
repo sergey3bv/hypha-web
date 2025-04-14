53654232a605cdd07b031bd2db306eab256902f2
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-14 13:46:42 +0300

    feat(#685): improved spaces schema

--- packages/storage-postgres/src/schema/space.ts
+++ packages/storage-postgres/src/schema/space.ts
@@ -2,6 +2,7 @@ import { InferInsertModel, InferSelectModel, relations } from 'drizzle-orm';
 import { integer, jsonb, pgTable, serial, text } from 'drizzle-orm/pg-core';
 import { commonDateFields } from './shared';
 import { categories } from './categories';
+import { AnyPgColumn } from 'drizzle-orm/pg-core';
 
 export const spaces = pgTable('spaces', {
   id: serial('id').primaryKey(),
@@ -15,8 +16,8 @@ export const spaces = pgTable('spaces', {
   categories: jsonb('categories')
     .$type<Array<(typeof categories.enumValues)[number]>>()
     .default([]),
-  parentId: integer('parent_id'),
   parentId: integer('parent_id').references((): AnyPgColumn => spaces.id),
+  ...commonDateFields,
 });
 
 export const spacesRelations = relations(spaces, ({ one, many }) => ({
