2859a3dd2a58316943b7cadcc88aad7fe75c6661
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-14 13:16:53 +0300

    feat(#685): fixed path

--- apps/web/src/hooks/use-member-by-slug.ts
+++ apps/web/src/hooks/use-member-by-slug.ts
@@ -3,7 +3,7 @@
 import React from 'react';
 import useSWR from 'swr';
 import { usePersonSlug } from './use-person-slug';
-import { useJwt } from '../../../../packages/core/src/people/client/hooks/useJwt';
+import { useJwt } from '@hypha-platform/core/client';
 
 export const useMemberBySlug = (slug: string) => {
   const { jwt } = useJwt();
