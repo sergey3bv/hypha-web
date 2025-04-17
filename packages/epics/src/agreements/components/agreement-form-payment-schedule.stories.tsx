57a2f291b2392065c41b25d8effca0e1232e59bd
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-17 14:35:22 +0300

    feat(#678): improved storybook

--- packages/epics/src/agreements/components/agreement-form-payment-schedule.stories.tsx
+++ packages/epics/src/agreements/components/agreement-form-payment-schedule.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { AgreementFormPaymentSchedule } from './agreement-form-payment-schedule';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-import { fn } from '@storybook/test';
 
 const meta = {
   component: AgreementFormPaymentSchedule,
@@ -15,9 +14,7 @@ export default meta;
 type Story = StoryObj<typeof AgreementFormPaymentSchedule>;
 
 export const Default: Story = {
-  args: {
-    onChange: fn(),
-  },
+  args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     expect(canvas.getByText(/Payment Schedule/gi)).toBeTruthy();
