5c2b137a1970a2ad7a484aeedbfaf5e8fa709df1
Philipp Litzenberger <philipp@bendcircular.com>
2025-04-17 11:20:28 +0200

    feat(#715): use type import

--- packages/epics/src/common/side-panel.tsx
+++ packages/epics/src/common/side-panel.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from 'react';
+
 type SidePanelProps = {
-  children: React.ReactNode;
+  children: ReactNode;
 };
 
 export const SidePanel = ({ children }: SidePanelProps) => {
