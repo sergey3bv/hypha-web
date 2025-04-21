eb689f386da2d8533517f1cf391001182b72dfd6
Philipp Litzenberger <philipp@bendcircular.com>
2025-04-21 10:35:27 +0200

    feat(#678): format

--- apps/web/src/app/[lang]/dho/[id]/_components/clean-path.ts
+++ apps/web/src/app/[lang]/dho/[id]/_components/clean-path.ts
@@ -5,10 +5,7 @@
  * @example
  * cleanPath('/de/dho/my-dho/agreements/something/else') => '/de/dho/my-dho/agreements'
  */
-export const cleanPath = (
-  pathname: string,
-  keepSegments = 4,
-): string => {
+export const cleanPath = (pathname: string, keepSegments = 4): string => {
   return pathname
     .split('/')
     .slice(0, keepSegments + 1)
