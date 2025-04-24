88b827c5f7326ae7005915ae5ef00cd2ef3d3c14
Philipp Litzenberger <philipp@bendcircular.com>
2025-04-24 09:03:54 +0200

    feat(#678): remove dummy plugin elements

--- packages/epics/src/agreements/plugins/propose-contribution/plugin.tsx
+++ packages/epics/src/agreements/plugins/propose-contribution/plugin.tsx
@@ -1,41 +1,15 @@
 'use client';
 
-import {
-  Separator,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@hypha-platform/ui';
-import { useFormContext } from 'react-hook-form';
-import { schemaProposeContribution } from './validation';
-import { z } from 'zod';
+import { Separator } from '@hypha-platform/ui';
 import { RecipientField } from './components/recipient-field';
 import { TokenPayoutFieldArray } from './components/token-payout-field-array';
 
 export const ProposeContributionPlugin = () => {
-  const methods = useFormContext<z.infer<typeof schemaProposeContribution>>();
-
   return (
-    <div>
-      <FormField
-        control={methods.control}
-        name="ProposeContributionPlugin"
-        render={() => {
-          return (
-            <FormItem>
-              <FormControl>
-                <div className="flex flex-col gap-4">
-                  <RecipientField />
-                  <Separator />
-                  <TokenPayoutFieldArray tokens={[]} name="payouts" />
-                </div>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          );
-        }}
-      />
+    <div className="flex flex-col gap-4">
+      <RecipientField />
+      <Separator />
+      <TokenPayoutFieldArray tokens={[]} name="payouts" />
     </div>
   );
 };
--- packages/epics/src/agreements/plugins/propose-contribution/validation.ts
+++ packages/epics/src/agreements/plugins/propose-contribution/validation.ts
@@ -3,11 +3,6 @@ import { z } from 'zod';
 const ETH_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/;
 
 export const schemaProposeContribution = z.object({
-  ProposeContributionPlugin: z
-    .string()
-    .min(1, { message: 'Contribution cannot be empty' })
-    .max(1000, { message: 'Contribution is too long (max 1000 characters)' })
-    .transform((val) => val.trim()),
   recipient: z
     .string()
     .min(1, { message: 'Recipient is required' })
