8c7dcab143e825ffcf6683c6262f13049d7e58fd
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-04-30 14:07:39 +0300

    feat(#680): updated types and validation

--- packages/core/src/governance/types.ts
+++ packages/core/src/governance/types.ts
@@ -15,9 +15,31 @@ export type Document = {
   creatorId: number;
   title: string;
   description?: string;
-  slug: string;
+  slug?: string;
   state: DocumentState;
   createdAt: Date;
   updatedAt: Date;
   creator?: Creator;
+  leadImage?: string;
+  attachments?: string[];
 };
+
+export interface CreateAgreementInput {
+  title: string;
+  description: string;
+  leadImage?: string;
+  attachments?: string[];
+  slug?: string;
+  spaceId: number;
+  creatorId: number;
+}
+
+export interface UpdateAgreementInput {
+  leadImage?: string;
+  slug?: string;
+  attachments?: string[];
+}
+
+export type UpdateAgreementBySlugInput = {
+  slug: string;
+} & UpdateAgreementInput;
