b08b5c60ffa043a957d9a0b5bd95614b6639fc46
Dmitry E <dsanichweb@gmail.com>
2025-05-12 19:29:39 +0300

    feat(#682): refactored payouts validation for proposals

--- packages/epics/src/agreements/plugins/pay-for-expenses/validation.ts
+++ packages/epics/src/agreements/plugins/pay-for-expenses/validation.ts
@@ -1,14 +1,8 @@
 import { z } from 'zod';
 import { recipientField } from '../components/common/recipient-field.validation';
+import { payoutsField } from '../components/common/token-payout-field-array.validation';
 
 export const schemaPayForExpenses = z.object({
   recipient: recipientField,
-  payouts: z
-    .array(
-      z.object({
-        amount: z.string().min(1, { message: 'Amount is required' }),
-        token: z.string().min(1, { message: 'Token is required' }),
-      }),
-    )
-    .min(1, { message: 'At least one payout is required' }),
+  payouts: payoutsField,
 });
