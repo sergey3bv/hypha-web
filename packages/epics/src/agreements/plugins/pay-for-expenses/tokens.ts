706bc077b8d475b9c25857df4996856205fe187e
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-14 16:10:07 +0300

    feat(#681): moved token icons to token folder in public

--- packages/epics/src/agreements/plugins/pay-for-expenses/tokens.ts
+++ packages/epics/src/agreements/plugins/pay-for-expenses/tokens.ts
@@ -3,22 +3,22 @@ import { type Token } from '../components/common/token-payout-field-array';
 export const tokens: Token[] = [
   {
     symbol: 'WETH',
-    icon: '/placeholder/weth-icon.png',
+    icon: '/icons/tokens/weth-icon.png',
     address: '0x4200000000000000000000000000000000000006',
   },
   {
     symbol: 'cbBTC',
-    icon: '/placeholder/cbBTC-icon.png',
+    icon: '/icons/tokens/cbBTC-icon.png',
     address: '0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf',
   },
   {
     symbol: 'USDC',
-    icon: '/placeholder/usdc-icon.png',
+    icon: '/icons/tokens/usdc-icon.png',
     address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
   },
   {
     symbol: 'EURC',
-    icon: '/placeholder/eurc-icon.png',
+    icon: '/icons/tokens/eurc-icon.png',
     address: '0x60a3E35Cc302bFA44Cb288Bc5a4F316Fdb1adb42',
   },
 ];
