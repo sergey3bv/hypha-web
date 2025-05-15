172c94b3a0b10eaefa5b7a121690b0ac74b4acd7
Dmitry E <dsanichweb@gmail.com>
2025-05-15 10:35:26 +0300

    feat(#682): restored label for pay for expences form

--- packages/epics/src/governance/components/create-pay-for-expenses-form.tsx
+++ packages/epics/src/governance/components/create-pay-for-expenses-form.tsx
@@ -136,6 +136,7 @@ export const CreatePayForExpensesForm = ({
             }}
             closeUrl={successfulUrl}
             isLoading={false}
+            label="Expenses"
           />
           {plugin}
           <Separator />
