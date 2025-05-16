db3ac809e9a7b26128f37888f2045579e75cc2f4
Dmitry E <dsanichweb@gmail.com>
2025-05-16 10:32:38 +0300

    feat(#682): improved reusage of token list

--- packages/epics/src/agreements/plugins/pay-for-expenses/plugin.tsx
+++ packages/epics/src/agreements/plugins/pay-for-expenses/plugin.tsx
@@ -3,6 +3,7 @@
 import { RecipientField } from '../components/common/recipient-field';
 import { TokenPayoutFieldArray } from '../components/common/token-payout-field-array';
 import { Separator } from '@hypha-platform/ui';
+<<<<<<< main
 import { tokens } from './tokens';
 import { Person } from '@core/people';
 
@@ -13,6 +14,12 @@ export const PayForExpensesPlugin = ({
   spaceSlug: string;
   members: Person[];
 }) => {
+=======
+import { useTokens } from '../hooks';
+
+export const PayForExpensesPlugin = () => {
+  const { tokens } = useTokens();
+>>>>>>> feat(#682): improved reusage of token list
   return (
     <div className="flex flex-col gap-4">
       <RecipientField recipients={members} />
