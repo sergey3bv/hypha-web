84e648b2776a7b2b55b89e3f041d217577383a37
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-22 15:02:57 +0300

    feat(#816): formatting

--- apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/[tab]/space-configuration/page.tsx
@@ -73,7 +73,7 @@ export default function SpaceConfiguration() {
             leadImage: space?.leadImage || '',
             categories: space?.categories || [],
             links: space?.links || [],
-            web3SpaceId: space?.web3SpaceId || undefined
+            web3SpaceId: space?.web3SpaceId || undefined,
           }}
         />
       </LoadingBackdrop>
