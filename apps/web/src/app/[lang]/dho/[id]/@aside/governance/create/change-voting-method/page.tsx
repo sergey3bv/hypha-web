88d10911173dabd7db2f8a061c77042863cac3d1
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-23 18:11:59 +0300

    feat(#658): added create-proposal-change-voting-method-form with base form fields

--- apps/web/src/app/[lang]/dho/[id]/@aside/governance/create/change-voting-method/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/governance/create/change-voting-method/page.tsx
@@ -1,5 +1,11 @@
-import { ButtonClose, SidePanel } from '@hypha-platform/epics';
+import {
+  SidePanel,
+  CreateProposalChangeVotingMethodForm,
+} from '@hypha-platform/epics';
 import { Locale } from '@hypha-platform/i18n';
+import { createSpaceService } from '@core/space/server';
+import { notFound } from 'next/navigation';
+import { getDhoPathGovernance } from '../../../../@tab/governance/constants';
 
 type PageProps = {
   params: Promise<{ lang: Locale; id: string }>;
@@ -8,10 +14,23 @@ type PageProps = {
 export default async function CreateChangeVotingMethodPage({
   params,
 }: PageProps) {
+  const { lang, id } = await params;
+
+  const spaceService = createSpaceService();
+
+  const spaceFromDb = await spaceService.getBySlug({ slug: id });
+
+  if (!spaceFromDb) notFound();
+  const { id: spaceId, web3SpaceId } = spaceFromDb;
+
   return (
     <SidePanel>
-      <ButtonClose dropSegment="select-settings-action" />
-      <div>Change voting method form</div>
+      <CreateProposalChangeVotingMethodForm
+        spaceId={spaceId}
+        web3SpaceId={web3SpaceId}
+        successfulUrl={getDhoPathGovernance(lang as Locale, id)}
+        plugin={<></>}
+      />
     </SidePanel>
   );
 }
