48330aa0e4140df5273af3fa7a7083dce6ff54a2
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-26 14:41:40 +0300

    feat(#658): implemented TokenSelectorField in ChangeVotingMethodPlugin

--- packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
+++ packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
@@ -5,6 +5,8 @@ import { VotingMethodSelector } from '../../components/voting-method-selector';
 import { Person } from '@core/people';
 import { MemberWithNumberFieldFieldArray } from '../components/common/member-with-number-field-array';
 import { DecaySettingsField } from '../components/common/decay-settings-field';
+import { TokenSelectorField } from '../components/common/token-selector-field';
+import { useTokens } from '@hypha-platform/epics';
 
 export const ChangeVotingMethodPlugin = ({
   spaceSlug,
@@ -14,15 +16,18 @@ export const ChangeVotingMethodPlugin = ({
   members: Person[];
 }) => {
   const [selectedMethod, setSelectedMethod] = useState<string | null>(null);
-
+  const { tokens } = useTokens();
   return (
     <div className="flex flex-col gap-4">
       <VotingMethodSelector onChange={setSelectedMethod} />
       {selectedMethod === '1v1v' && (
         <>
-          <MemberWithNumberFieldFieldArray members={members} />
+          <MemberWithNumberFieldFieldArray name="members" members={members} />
           <DecaySettingsField name="decaySettings" />
         </>
+      )}
+      {selectedMethod === '1t1v' && (
+        <TokenSelectorField name="token" tokens={tokens} />
       )}
     </div>
   );
--- packages/epics/src/agreements/plugins/components/common/token-selector.tsx
+++ packages/epics/src/agreements/plugins/components/common/token-selector.tsx
@@ -33,48 +33,53 @@ export const TokenSelector = ({
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex justify-between items-center gap-2 min-w-[140px] px-4 py-2 border border-neutral-7 rounded-md hover:bg-neutral-3 cursor-pointer">
-          <div className="flex items-center gap-2">
-            {selectedToken ? (
-              <>
+    <div className="flex justify-between w-full">
+      <label className="text-3 text-neutral-11 flex items-center">Token</label>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex justify-between items-center gap-2 min-w-[140px] px-4 py-2 border border-neutral-7 rounded-md hover:bg-neutral-3 cursor-pointer">
+              <div className="flex items-center gap-2">
+                {selectedToken ? (
+                  <>
+                    <Image
+                      src={selectedToken.icon}
+                      width={20}
+                      height={20}
+                      alt={selectedToken.symbol}
+                    />
+                    <span className="text-2 text-neutral-11">
+                      {selectedToken.symbol}
+                    </span>
+                  </>
+                ) : (
+                  <span className="text-2 text-neutral-11 whitespace-nowrap">
+                    Select a token
+                  </span>
+                )}
+              </div>
+              <ChevronDownIcon />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {tokens.map((token) => (
+              <DropdownMenuItem
+                key={token.address}
+                onSelect={() => handleTokenChange(token)}
+              >
                 <Image
-                  src={selectedToken.icon}
-                  width={20}
-                  height={20}
-                  alt={selectedToken.symbol}
+                  src={token.icon}
+                  width={24}
+                  height={24}
+                  alt={token.symbol}
+                  className="mr-2"
                 />
-                <span className="text-2 text-neutral-11">
-                  {selectedToken.symbol}
-                </span>
-              </>
-            ) : (
-              <span className="text-2 text-neutral-11 whitespace-nowrap">
-                Select a token
-              </span>
-            )}
-          </div>
-          <ChevronDownIcon />
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        {tokens.map((token) => (
-          <DropdownMenuItem
-            key={token.address}
-            onSelect={() => handleTokenChange(token)}
-          >
-            <Image
-              src={token.icon}
-              width={24}
-              height={24}
-              alt={token.symbol}
-              className="mr-2"
-            />
-            <span className="text-2 text-neutral-11">{token.symbol}</span>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+                <span className="text-2 text-neutral-11">{token.symbol}</span>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   );
 };
