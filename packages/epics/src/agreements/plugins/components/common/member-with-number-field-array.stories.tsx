cc313104af30abf3227d238df3889e70bdb02a94
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-26 12:14:50 +0300

    feat(#658): created member-with-number-field-array component

---
+++ packages/epics/src/agreements/plugins/components/common/member-with-number-field-array.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MemberWithNumberFieldFieldArray } from './member-with-number-field-array';
+import { Form } from '@hypha-platform/ui';
+import { useForm } from 'react-hook-form';
+import { ReactNode } from 'react';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const FormDecorator = ({ children }: { children: ReactNode }) => {
+  const methods = useForm({
+    defaultValues: {
+      members: [],
+    },
+  });
+
+  return <Form {...methods}>{children}</Form>;
+};
+
+const meta = {
+  component: MemberWithNumberFieldFieldArray,
+  title: 'Epics/Agreements/MemberWithNumberFieldFieldArray',
+  decorators: [
+    (Story) => (
+      <FormDecorator>
+        <Story />
+      </FormDecorator>
+    ),
+  ],
+} satisfies Meta<typeof MemberWithNumberFieldFieldArray>;
+
+export default meta;
+
+type Story = StoryObj<typeof MemberWithNumberFieldFieldArray>;
+
+export const Default: Story = {
+  args: {
+    members: [
+      {
+        name: 'Charlie',
+        surname: 'Chaplin',
+        avatarUrl: 'https://github.com/shadcn.png',
+        address: '0xc8B8454D2F9192FeCAbc2C6F5d88F6434A2a9cd1',
+      },
+      {
+        name: 'Diana',
+        surname: 'Doe',
+        avatarUrl: 'https://github.com/shadcn.png',
+        address: '0xc8B8454D2F9192FeCAbc2C6F5d88F6434A2a9cd4',
+      },
+    ],
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Initial voice allocation/i)).toBeTruthy();
+  },
+};
---
+++ packages/epics/src/agreements/plugins/components/common/member-with-number-field-array.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useFormContext, useFieldArray } from 'react-hook-form';
+import { MemberWithNumberField } from './member-with-number';
+import {
+  Button,
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+  Separator,
+} from '@hypha-platform/ui';
+import { Cross2Icon, PlusIcon } from '@radix-ui/react-icons';
+import { Text } from '@radix-ui/themes';
+import { Person } from '@core/people';
+
+interface MemberWithNumberFieldFieldArrayProps {
+  members: Person[];
+  name?: string;
+}
+
+export const MemberWithNumberFieldFieldArray = ({
+  members,
+  name = 'members',
+}: MemberWithNumberFieldFieldArrayProps) => {
+  const { control } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name,
+  });
+  console.log('members', members);
+  const handleAddField = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    append({ member: null, number: '' });
+  };
+
+  return (
+    <div className="flex flex-col gap-5">
+      <Separator />
+      <Text className="text-3 text-neutral-11">Initial voice allocation</Text>
+      <div className="flex flex-col gap-2 w-full items-end">
+        {fields.map((field, index) => (
+          <div key={field.id} className="flex items-center gap-2">
+            <div className="flex-1">
+              <FormField
+                control={control}
+                name={`${name}.${index}`}
+                render={({ field: { value, onChange } }) => (
+                  <FormItem>
+                    <FormControl>
+                      <MemberWithNumberField
+                        members={members}
+                        value={value}
+                        onChange={onChange}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button variant="ghost" onClick={() => remove(index)}>
+              <Cross2Icon />
+            </Button>
+          </div>
+        ))}
+        <div className="flex justify-end w-full">
+          <Button className="w-fit" onClick={handleAddField} variant="ghost">
+            <PlusIcon />
+            Add
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
