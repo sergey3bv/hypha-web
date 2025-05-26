49ab7ad2601a51feef9c6dd38302515d1112cc9d
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-26 13:00:38 +0300

    feat(#658): implemented DecaySettingsField in ChangeVotingMethodPlugin

--- packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
+++ packages/epics/src/agreements/plugins/change-voting-method/plugin.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { VotingMethodSelector } from '../../components/voting-method-selector';
 import { Person } from '@core/people';
 import { MemberWithNumberFieldFieldArray } from '../components/common/member-with-number-field-array';
+import { DecaySettingsField } from '../components/common/decay-settings-field';
 
 export const ChangeVotingMethodPlugin = ({
   spaceSlug,
@@ -18,7 +19,10 @@ export const ChangeVotingMethodPlugin = ({
     <div className="flex flex-col gap-4">
       <VotingMethodSelector onChange={setSelectedMethod} />
       {selectedMethod === '1v1v' && (
-        <MemberWithNumberFieldFieldArray members={members} />
+        <>
+          <MemberWithNumberFieldFieldArray members={members} />
+          <DecaySettingsField name="decaySettings" />
+        </>
       )}
     </div>
   );
