b164dd396ae0bd10139ee8aa6e18fb4e51becc33
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2025-05-27 15:17:57 +0300

    feat(#663): created IssueNewTokenForm with base fields

--- apps/web/src/app/[lang]/dho/[id]/@aside/treasury/create/issue-new-token/page.tsx
+++ apps/web/src/app/[lang]/dho/[id]/@aside/treasury/create/issue-new-token/page.tsx
@@ -3,6 +3,7 @@ import { Locale } from '@hypha-platform/i18n';
 import { createSpaceService } from '@core/space/server';
 import { notFound } from 'next/navigation';
 import { getDhoPathTreasury } from '../../../../@tab/treasury/constants';
+import { IssueNewTokenForm } from '@hypha-platform/epics';
 
 type PageProps = {
   params: Promise<{ lang: Locale; id: string }>;
@@ -16,7 +17,17 @@ export default async function IssueNewTokenPage({ params }: PageProps) {
   const spaceFromDb = await spaceService.getBySlug({ slug: id });
 
   if (!spaceFromDb) notFound();
+
   const { id: spaceId, web3SpaceId, slug: spaceSlug } = spaceFromDb;
 
-  return <SidePanel>Issue new token form</SidePanel>;
+  return (
+    <SidePanel>
+      <IssueNewTokenForm
+        spaceId={spaceId}
+        web3SpaceId={web3SpaceId}
+        successfulUrl={getDhoPathTreasury(lang as Locale, id)}
+        plugin={<></>}
+      />
+    </SidePanel>
+  );
 }
